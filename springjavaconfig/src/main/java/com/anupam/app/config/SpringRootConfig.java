2b40f53aabfa3d39beed85e29124c5c4a652f051
anupamgogoi0907 <anupamgogoi0907@gmail.com>
2016-08-31 15:34:21 -0300

    asa
    sasas

---
+++ springjavaconfig/src/main/java/com/anupam/app/config/SpringRootConfig.java
@@ -0,0 +1,10 @@
+package com.anupam.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = { "com.anupam.app.dao", "com.anupam.app.service" })
+public class SpringRootConfig {
+
+}
